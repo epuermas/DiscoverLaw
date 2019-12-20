705a1ddaa3c86c7b5b0826cddd2c03ca53ed7aa4
Eduardo Puermas <eduardopuermas@hotmail.com>
2019-12-20 00:09:28 +0000

    Initial commit

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,14 +3,38 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
+		<nav className="navbar navbar-expand-lg navbar-light bg-light">
+			<button
+				className="navbar-toggler"
+				type="button"
+				data-toggle="collapse"
+				data-target="#navbarNav"
+				aria-controls="navbarNav"
+				aria-expanded="false"
+				aria-label="Toggle navigation">
+				<span className="navbar-toggler-icon" />
+			</button>
+			<div className="collapse navbar-collapse" id="navbarNav">
+				<ul className="navbar-nav">
+					<img
+						src="https://i.imgur.com/Hkm9zHE.png"
+						width="100%"
+						alt=""
+						className="d-inline-block align-middle mr-2"
+					/>
+
+					<li className="nav-item active">
+						<a className="nav-link" href="#">
+							Home <span className="sr-only">(current)</span>
+						</a>
+					</li>
+
+					<li className="nav-item">
+						<a className="nav-link" href="#">
+							Features
+						</a>
+					</li>
+				</ul>
 			</div>
 		</nav>
 	);
