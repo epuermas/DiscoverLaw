400c88b84b426268a7a143dbc610694f6ddd3588
Eduardo Puermas <eduardopuermas@hotmail.com>
2020-01-31 02:05:15 +0000

    working on footer

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -1,10 +1,35 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { Demo } from "../views/demo";
+import ReactDOM from "react-dom";
+import { SocialIcon } from "react-social-icons";
+import "../../styles/index.scss";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3">
-		<Link to={""} className="about-button" href="#">
+	<footer
+		className="footer mt-auto py-3"
+		style={{
+			position: "absolute",
+			width: "100%",
+			height: "200px",
+			bottom: "0"
+		}}>
+		<div
+			style={{
+				position: "absolute",
+				width: "25%",
+				height: "100%",
+				backgroundColor: "green",
+				left: "75%"
+			}}>
+			<SocialIcon
+				style={{ margin: "5px", position: "absolute", right: "0", color: "black" }}
+				url="http://twitter.com/jaketrent"
+			/>
+		</div>
+	</footer>
+);
+/* <Link to={""} className="about-button" href="#">
 			About Us
 		</Link>
 
@@ -16,9 +41,12 @@ export const Footer = () => (
 			Ask A Lawyer
 		</Link>
 
-		<p>
+		{/* <p>
 			<br />
 			Made with <i className="fa fa-heart text-danger text-center" /> by E.Puermas, J.Guzman, Abel
-		</p>
-	</footer>
-);
+        </p> */
+
+// <p>
+// 	<br />
+// 	<SocialIcon url="http://twitter.com/jaketrent" />
+// </p>
