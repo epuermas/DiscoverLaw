ba0f788fcf44d7662aaae1d9e100bf5fee0f8f7f
Eduardo Puermas <eduardopuermas@hotmail.com>
2020-01-31 01:12:31 +0000

    character counter added

--- src/js/views/AskALawyer.js
+++ src/js/views/AskALawyer.js
@@ -4,6 +4,8 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { TextField } from "@material-ui/core";
 
 export const AskALawyer = () => {
+	const maxLength = 1000;
+	const [question, setQuestion] = useState("");
 	return (
 		<form>
 			<div className="ask-form flex-column h-100">
@@ -11,16 +13,21 @@ export const AskALawyer = () => {
 					id="standard-full-width"
 					style={{ margin: 6 }}
 					placeholder="Write your question"
-					helperText="Normally an attorney will respond within 24 hours"
+					helperText={"You have " + (maxLength - question.length) + " left"}
 					fullWidth
 					margin="dense"
 					variant="outlined"
 					multiline="true"
+					defaultValue={question}
+					onChange={e => setQuestion(e.target.value)}
 					InputLabelProps={{
 						shrink: true
 					}}
 				/>
 			</div>
+			<a className="send-email-button btn-primary btn-lg" href="#" role="button">
+				About Us
+			</a>
 		</form>
 	);
 };
